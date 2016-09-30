8f378ce0d33b14ad9a2eb0101c94e8b310258b3a
Timo <timo.mynttinen@mamk.fi>
2016-09-30 14:47:48 +0300

    Added index.html

--- src/WAConflict/Startup.cs
+++ src/WAConflict/Startup.cs
@@ -70,19 +70,19 @@ namespace WAConflict
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
